0a84ec2a3ffcaf9dc01d75f814ae50e8ef3c2966
Evelin Kolev <evelinkolev@vivaldi.net>
2024-05-07 14:12:40 +0300

    Get yesterday's closing stock price

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import requests
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "58V6SYR39S82ET3G"
+NEWS_API_KEY = "8c737d07a64549b1ad7b12d359c853e3"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
