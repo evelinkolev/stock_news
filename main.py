c81c58fa6a59dc029e389c3436715662142b5b8d
Evelin Kolev <evelinkolev@vivaldi.net>
2024-05-08 15:44:32 +0300

    Send each article as a separate message via Twilio

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from twilio.rest import Client
 import requests
 
 STOCK = "TSLA"
@@ -9,6 +9,12 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "58V6SYR39S82ET3G"
 NEWS_API_KEY = "8c737d07a64549b1ad7b12d359c853e3"
+
+TWILIO_SID = "YOUR TWILIO ACCOUNT SID"
+TWILIO_AUTH_TOKEN = "YOUR TWILIO AUTH TOKEN"
+
+VIRTUAL_TWILIO_NUMBER = "your virtual twilio number"
+VERIFIED_NUMBER = "your own phone number verified with Twilio"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -30,7 +36,13 @@ print(day_before_yesterday_closing_price)
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
 
-difference_percent = (difference / float(yesterday_closing_price)) * 100
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+difference_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(difference_percent)
 
 if difference_percent > 3:
@@ -44,3 +56,14 @@ if difference_percent > 3:
 
     three_articles = articles[:3]
     print(three_articles)
+
+    formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
